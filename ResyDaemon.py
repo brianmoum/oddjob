edbeb27f88f7e005887b442fc85163c69a0513fc
Brian Moum <bmoum@yext.com>
2024-04-24 13:57:47 -0400

    First Commit

---
+++ ResyDaemon.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import pandas as pd, requests, io, re, csv, datetime, sys, json, mysql.connector, html
+
+import time 
+ 
+import pandas as pd 
+from selenium import webdriver 
+from selenium.webdriver import Chrome 
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By 
+from webdriver_manager.chrome import ChromeDriverManager
+
+def main() :
+	pass
